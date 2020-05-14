d54616d5ff9f441d8b0713d894082f012ef22e05
Helen Chen <hc20@princeton.edu>
2020-05-13 22:59:33 -0400

    added google fonts and just name

---
+++ helenellenxy/helenellen/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+	context = {}
+	# for some reason cannot be templates/helenellen/home.html
+	return render(request, 'helenellen/home.html', context)
