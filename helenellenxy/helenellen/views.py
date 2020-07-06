4caf0f6d8875655680e5105aa8161ea24df6e404
Helen Chen <hc20@princeton.edu>
2020-07-06 18:28:51 -0400

    delete all

--- helenellenxy/helenellen/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def home(request):
-	context = {}
-	# for some reason cannot be templates/helenellen/home.html
-	return render(request, 'helenellen/index.html', context)
